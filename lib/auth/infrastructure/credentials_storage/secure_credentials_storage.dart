0971af9c0d8287e4adac5ef8cdb3df0af505b78e
Rashidul420222 <rashidul420222@gmail.com>
2021-11-06 09:34:25 +0600

    Secure Credentials Storage added

--- lib/auth/infrastructure/credentials_storage/secure_credentials_storage.dart
+++ lib/auth/infrastructure/credentials_storage/secure_credentials_storage.dart
@@ -4,24 +4,38 @@ import 'package:oauth2/src/credentials.dart';
 
 class SecureCredentialsStorage implements CredentialsStorage {
   final FlutterSecureStorage _storage;
-
   SecureCredentialsStorage(this._storage);
 
+  static const _key = 'oauth2_credentials';
+  Credentials? _cachedCredentials;
+
   @override
-  Future<Credentials?> read() {
-    // TODO: implement read
-    throw UnimplementedError();
+  Future<Credentials?> read() async {
+    if (_cachedCredentials != null) {
+      return _cachedCredentials;
+    }
+
+    final json = await _storage.read(key: _key);
+    if (json == null) {
+      return null;
+    }
+
+    try {
+      return _cachedCredentials = Credentials.fromJson(json);
+    } on FormatException {
+      return null;
+    }
   }
 
   @override
   Future<void> save(Credentials credentials) {
-    // TODO: implement save
-    throw UnimplementedError();
+    _cachedCredentials = credentials;
+    return _storage.write(key: _key, value: credentials.toJson());
   }
 
   @override
   Future<void> clear() {
-    // TODO: implement clear
-    throw UnimplementedError();
+    _cachedCredentials = null;
+    return _storage.delete(key: _key);
   }
 }
