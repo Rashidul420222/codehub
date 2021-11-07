8ca30f5554e7414abb415e08e2d77316366dbecd
Rashidul420222 <rashidul420222@gmail.com>
2021-11-07 10:14:03 +0600

    link setup

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,5 +1,36 @@
+import 'package:codehub/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:flutter/services.dart';
 import 'package:oauth2/oauth2.dart';
 
 class GithubAuthenticator {
-  Future<Credentials> getSignedInCredentials()  async {}
+  final CredentialsStorage _credentialsStorage;
+
+  GithubAuthenticator(this._credentialsStorage);
+
+  static const clientId = '52a64130377ac1c3a4ba';
+  static const clientSecrets = 'd0f3061ab4b3d0ca04af9ef0c7b0bef1b6fe5168';
+  static const scopes = ['read:user', 'repo'];
+  static final authorizationEndpoint =
+      Uri.parse('https://github.com/login/oauth/authorize');
+  static final tokenEndpoint =
+      Uri.parse('https://github.com/login/oauth/access_token');
+  static final redirectUrl = Uri.parse('http://localhost:3000/callback');
+
+  Future<Credentials?> getSignedInCredentials() async {
+    try {
+      final storedCredentials = await _credentialsStorage.read();
+
+      if (storedCredentials != null) {
+        if (storedCredentials.canRefresh && storedCredentials.isExpired) {
+          // TODO: refresh
+        }
+        return storedCredentials;
+      }
+    } on PlatformException {
+      return null;
+    }
+  }
+
+  Future<bool> isSignedIn() =>
+      getSignedInCredentials().then((credentials) => credentials != null);
 }
