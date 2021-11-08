efd90901a2c90a70be44003f33594981fb08222c
Rashidul420222 <rashidul420222@gmail.com>
2021-11-08 14:59:13 +0600

    76

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,6 +1,18 @@
+import 'package:codehub/auth/domain/auth_failure.dart';
 import 'package:codehub/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter/services.dart';
 import 'package:oauth2/oauth2.dart';
+import 'package:http/http.dart' as http;
+
+class GithubOAuthHttpClient extends http.BaseClient {
+  final httpClient = http.Client();
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Accept'] = 'application/json';
+    return httpClient.send(request);
+  }
+}
 
 class GithubAuthenticator {
   final CredentialsStorage _credentialsStorage;
@@ -21,9 +33,7 @@ class GithubAuthenticator {
       final storedCredentials = await _credentialsStorage.read();
 
       if (storedCredentials != null) {
-        if (storedCredentials.canRefresh && storedCredentials.isExpired) {
-          // TODO: refresh
-        }
+        if (storedCredentials.canRefresh && storedCredentials.isExpired) {}
         return storedCredentials;
       }
     } on PlatformException {
@@ -33,4 +43,35 @@ class GithubAuthenticator {
 
   Future<bool> isSignedIn() =>
       getSignedInCredentials().then((credentials) => credentials != null);
+
+  AuthorizationCodeGrant codeGrant() {
+    return AuthorizationCodeGrant(
+      clientId,
+      authorizationEndpoint,
+      tokenEndpoint,
+      secret: clientSecrets,
+      httpClient: GithubOAuthHttpClient(),
+    );
+  }
+
+  Uri getAuthorizationUrl(AuthorizationCodeGrant grant) {
+    return grant.getAuthorizationUrl(redirectUrl, scopes: scopes);
+  }
+
+  Future<Either<AuthFailure, Unit>> HandleAuthorizationResponse(
+    AuthorizationCodeGrant grant,
+    Map<String, String> queryParams,
+  ) async {
+    try {
+      final httpClient = await grant.handleAuthorizationResponse(queryParams);
+      await _credentialsStorage.save(httpClient.credentials);
+      return right(unit);
+    } on FormatException {
+      return left(const AuthFailure.server());
+    } on AuthorizationException catch (e) {
+      return left(AuthFailure.server('${e.error} : ${e.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
 }
